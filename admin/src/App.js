4b4a89880f69eaf1dd16c9f8324ac8c3e99e8888
Sebastian Kurfürst <sebastian.kurfuerst@sandstorm.de>
2020-05-04 15:08:31 +0200

    TASK: apply 05_add_prod_build.patch

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
 import { HydraAdmin } from '@api-platform/admin';
 
-export default () => <HydraAdmin entrypoint={process.env.REACT_APP_API_ENTRYPOINT}/>;
+// NOTE: the "/api" endpoint is not allowed to end with a slash for proper functioning
+export default () => <HydraAdmin entrypoint={process.env.REACT_APP_API_ENTRYPOINT || window.location.origin + '/api'}/>;
--- client/src/config/entrypoint.js
+++ client/src/config/entrypoint.js
@@ -1,1 +1,2 @@
-export const ENTRYPOINT = process.env.REACT_APP_API_ENTRYPOINT;
+// NOTE: this entrypoint URL MUST end with /
+export const ENTRYPOINT = process.env.REACT_APP_API_ENTRYPOINT || window.location.origin + "/api/";
