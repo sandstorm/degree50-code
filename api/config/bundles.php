b763f3539a66b9827d3d47629278b28e94da30ad
Sebastian Kurfürst <sebastian.kurfuerst@sandstorm.de>
2020-05-07 17:50:38 +0200

    FEATURE: add oneup/uploader for huge file uploads

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -18,4 +18,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Oneup\FlysystemBundle\OneupFlysystemBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
+    Oneup\UploaderBundle\OneupUploaderBundle::class => ['all' => true],
 ];
