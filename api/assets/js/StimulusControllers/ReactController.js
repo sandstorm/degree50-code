40c86eafb4bb004352b6c17b1fb439efbb502943
Sebastian Kurfürst <sebastian.kurfuerst@sandstorm.de>
2020-05-14 12:24:01 +0200

    BUGFIX: fix js code reloading

--- api/assets/js/StimulusControllers/ReactController.js
+++ api/assets/js/StimulusControllers/ReactController.js
@@ -3,16 +3,12 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 
 import widgets from '../Widgets/Index';
-
 export default class extends Controller {
   connect() {
-    console.log("CONNECT434");
     const widgetName = this.data.get('widget');
-    console.log(widgetName);
     const propsAsString = this.data.get('props');
 
     const ReactWidget = widgets[widgetName];
-    console.log("ReactWidget", ReactWidget);
     const props = propsAsString ? JSON.parse(propsAsString) : {};
     ReactDOM.render(<ReactWidget {...props} />, this.element);
   }
