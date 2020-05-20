d4f7627d9091380806622307e0414cde022fb950
Michael Berhorst <michael.berhorst@sandstorm-media.de>
2020-05-20 16:15:54 +0200

    User has to be logged in for some controllers

--- api/src/Controller/IndexController.php
+++ api/src/Controller/IndexController.php
@@ -6,7 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
+/**
+ * @IsGranted("ROLE_USER")
+ */
 class IndexController extends AbstractController
 {
     /**
