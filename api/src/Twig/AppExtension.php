ddfef49826550e579aefd3e2bc474934a80b987e
Michael Berhorst <michael.berhorst@sandstorm-media.de>
2020-10-29 12:51:58 +0100

    Sidebar status is saved in cookies for the session

--- api/src/Twig/AppExtension.php
+++ api/src/Twig/AppExtension.php
@@ -4,15 +4,23 @@
 namespace App\Twig;
 
 use Twig\Extension\AbstractExtension;
+use Twig\Extension\GlobalsInterface;
 use Twig\TwigFilter;
 
-class AppExtension extends AbstractExtension
+class AppExtension extends AbstractExtension implements GlobalsInterface
 {
     public function getFilters()
     {
         return [
             // the logic of this filter is now implemented in a different class
             new TwigFilter('virtualized_file_url', [AppRuntime::class, 'virtualizedFileUrl']),
+        ];
+    }
+
+    public function getGlobals(): array
+    {
+        return [
+            'sidebar_is_open' => key_exists("sidebarIsOpen", $_COOKIE) && $_COOKIE["sidebarIsOpen"] === "false" ? 'sidebar--is-closed' : '',
         ];
     }
 }
