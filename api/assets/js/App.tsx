185ce79ac49c2c700588ac9d3c0e944565c276f8
Robert Baruck <robert.baruck@sandstorm.de>
2022-01-27 13:32:55 +0100

    TASK: Revert Bootstrap upgrade and remove bootstrap js from App

--- api/assets/js/App.tsx
+++ api/assets/js/App.tsx
@@ -9,7 +9,6 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import '../scss/App.scss'
-import 'bootstrap'
 import { Application } from 'stimulus'
 import VideoUploadController from './StimulusControllers/VideoUploadController'
 import SubtitlesUploadController from './StimulusControllers/SubtitlesUploadController'
