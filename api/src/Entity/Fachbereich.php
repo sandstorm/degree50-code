f05c55dc5192b6e9251ed28cab8e8605e44da3ab
Robert Baruck <robert.baruck@sandstorm.de>
2022-04-05 14:38:10 +0200

    TASK: Create Fachbereich Entity & Administration

---
+++ api/src/Entity/Fachbereich.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Entity;
+
+use App\Core\EntityTraits\IdentityTrait;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity
+ */
+class Fachbereich
+{
+    use IdentityTrait;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column
+     * @Assert\NotBlank
+     */
+    private string $name = '';
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function __construct(?string $id = null)
+    {
+        $this->generateOrSetId($id);
+    }
+}
