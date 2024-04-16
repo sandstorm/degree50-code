d5f68cfb24cfd2e881d81cffd4c78f5de81a5f69
Robert Baruck <robert.baruck@sandstorm.de>
2024-04-16 13:52:06 +0200

    TASK: Add web profiler

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -21,4 +21,5 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
