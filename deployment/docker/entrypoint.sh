113a82dc16c022edaeb8f59f2dc0a682eb16b23f
Robert Baruck <robert.baruck@sandstorm.de>
2024-04-19 11:46:14 +0200

    WIP: docker entry point

--- deployment/docker/entrypoint.sh
+++ deployment/docker/entrypoint.sh
@@ -41,7 +41,7 @@ until bin/console doctrine:query:sql "SELECT 1" > /dev/null 2>&1; do
 	sleep 1
 done
 
-if ls -A src/Migrations/*.php > /dev/null 2>&1; then
+if ls -A migrations/*.php > /dev/null 2>&1; then
 	bin/console doctrine:migrations:migrate --no-interaction || echo "!!!!!!!!! ERROR running Doctrine migrations. Run them after starting and fix the errors."
 fi
 
